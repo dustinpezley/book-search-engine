1710f126b05879d63f0070c221aee0e92eadefe1
dustinpezley <dustinpezley@gmail.com>
2022-09-23 20:05:45 -0500

    updated server.js, added schemas, updated server/utils/auth

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const { request } = require('express');
 
 // set token secret and expiration date
 const secret = 'mysecretsshhhhh';
@@ -6,9 +7,9 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -29,7 +30,8 @@ module.exports = {
     }
 
     // send to next endpoint
-    next();
+    // next();
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
